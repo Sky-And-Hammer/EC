7f987fef50c25258b3c50450fca6381ef1cb5db0
GloomySunday <121055230@qq.com>
2017-01-11 15:59:51 +0800

    add

---
+++ config.go
@@ -0,0 +1,10 @@
+package EC
+
+import (
+	//  The fantastic ORM library for Golang, aims to be developer friendly.
+	"github.com/jinzhu/gorm"
+)
+
+type Config struct {
+	DB *gorm.DB
+}
---
+++ context.go
@@ -0,0 +1,45 @@
+package EC
+
+import (
+	"net/http"
+
+	//  The fantastic ORM library for Golang, aims to be developer friendly.
+	"github.com/jinzhu/gorm"
+)
+
+//	'CurrentUser' is an interface, which is used for ec admin to get current logged user
+type CurrentUser interface {
+	DisplayName() string
+}
+
+//	'Context' is ec context, which is used for many ec components, used to share infoation between them
+type Context struct {
+	Request     *http.Request
+	Writer      *http.ResponseWriter
+	ResourceID  string
+	Config      *Config
+	Roles       []string
+	DB          *gorm.DB
+	CurrentUser CurrentUser
+	Errors
+}
+
+//	'Clone' clone current context
+func (context *Context) Clone() *Context {
+	var clone = *context
+	return &clone
+}
+
+//	'GetDB' get db from current context
+func (context *Context) GetDB() *gorm.DB {
+	if context.DB != nil {
+		return context.DB
+	}
+
+	return context.Config.DB
+}
+
+//	'SetDB' set db into current context
+func (context *Context) SetDB(DB *gorm.DB) {
+	context.DB = DB
+}
