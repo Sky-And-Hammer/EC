855570837937d95d8bb801cb96ffa5106b956b40
GloomySunday <121055230@qq.com>
2017-01-11 16:24:15 +0800

    fix

---
+++ errors.go
@@ -0,0 +1,43 @@
+package TM_EC
+
+import (
+	"strings"
+)
+
+type Errors struct {
+	errors []error
+}
+
+func (errs Errors) Error() string {
+	var errors []string
+	for _, err := range errs.errors {
+		errors = append(errors, err.Error())
+	}
+
+	return strings.Join(errors, ";")
+}
+
+func (errs *Errors) AddError(errors ...error) {
+	for _, err := range errs.errors {
+		if err != nil {
+			if e, ok := err.(errorsInterface); ok {
+				errs.errors = append(errs.errors, e.GetErrors()...)
+			} else {
+				errs.errors = append(errs.errors, err)
+			}
+		}
+	}
+}
+
+func (errs Errors) HasError() bool {
+	return len(errs.errors) != 0
+}
+
+//	'getErrors'
+func (errs Errors) getErrors() []error {
+	return errs.getErrors()
+}
+
+type errorsInterface interface {
+	GetErrors() []error
+}
