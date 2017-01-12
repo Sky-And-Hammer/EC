fa7e8b13558c21f92d146b3d29f7a7dc2bfd0696
GloomySunday <121055230@qq.com>
2017-01-12 11:23:22 +0800

    add file

--- utils/utils_test.go
+++ utils/utils_test.go
@@ -27,3 +27,108 @@ func TestHumanizeString(t *testing.T) {
 		}
 	}
 }
+
+func TestToParamString(t *testing.T) {
+	cases := [][2]string{
+		{"OrderItem", "order_item"},
+		{"order item", "order_item"},
+		{"Order Item", "order_item"},
+		{"FAQ", "faq"},
+		{"FAQPage", "faq_page"},
+	}
+
+	for _, c := range cases {
+		if got := ToParamString(c[0]); got != c[1] {
+			t.Errorf("ToParamString(%q) = %q; want %q", c[0], got, c[1])
+		}
+	}
+}
+
+func TestPatchURL(t *testing.T) {
+	cases := []struct {
+		original string
+		input    []interface{}
+		want     string
+		err      error
+	}{
+		{
+			original: "http://qor.com/admin/orders?locale=global&q=dotnet&test=1#test",
+			input:    []interface{}{"locale", "cn"},
+			want:     "http://qor.com/admin/orders?locale=cn&q=dotnet&test=1#test",
+		},
+		{
+			original: "http://qor.com/admin/orders?locale=global&q=dotnet&test=1#test",
+			input:    []interface{}{"locale", ""},
+			want:     "http://qor.com/admin/orders?q=dotnet&test=1#test",
+		},
+	}
+	for _, c := range cases {
+		// u, _ := url.Parse(c.original)
+		// context := Context{Context: &qor.Context{Request: &http.Request{URL: u}}}
+		got, err := PatchURL(c.original, c.input...)
+		if c.err != nil {
+			if err == nil || err.Error() != c.err.Error() {
+				t.Errorf("got error %s; want %s", err, c.err)
+			}
+		} else {
+			if err != nil {
+				t.Error(err)
+			}
+			if got != c.want {
+				t.Errorf("context.PatchURL = %s; c.want %s", got, c.want)
+			}
+		}
+	}
+}
+
+func TestJoinURL(t *testing.T) {
+	var cases = []struct {
+		original string
+		input    []interface{}
+		want     string
+		err      error
+	}{
+		{
+			original: "http://qor.com",
+			input:    []interface{}{"admin"},
+			want:     "http://qor.com/admin",
+		},
+		{
+			original: "http://qor.com",
+			input:    []interface{}{"/admin"},
+			want:     "http://qor.com/admin",
+		},
+		{
+			original: "http://qor.com/",
+			input:    []interface{}{"/admin"},
+			want:     "http://qor.com/admin",
+		},
+		{
+			original: "http://qor.com?q=keyword",
+			input:    []interface{}{"admin"},
+			want:     "http://qor.com/admin?q=keyword",
+		},
+		{
+			original: "http://qor.com/?q=keyword",
+			input:    []interface{}{"admin"},
+			want:     "http://qor.com/admin?q=keyword",
+		},
+	}
+	for _, c := range cases {
+		// u, _ := url.Parse(c.original)
+		// context := Context{Context: &qor.Context{Request: &http.Request{URL: u}}}
+		got, err := JoinURL(c.original, c.input...)
+		if c.err != nil {
+			if err == nil || err.Error() != c.err.Error() {
+				t.Errorf("got error %s; want %s", err, c.err)
+			}
+		} else {
+			if err != nil {
+				t.Error(err)
+			}
+			if got != c.want {
+				t.Errorf("context.JoinURL = %s; c.want %s", got, c.want)
+			}
+		}
+	}
+}
